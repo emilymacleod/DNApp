574093dcdbf135aaca7595f23cf8583929b11ce5
emilymacleod <margaretemacleod@gmail.com>
2015-05-20 14:34:07 -0300

    added loginviewcontroller
    added loginviewcontroller

---
+++ DNApp/LogInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LogInViewController.swift
+//  DNApp
+//
+//  Created by Emily MacLeod on 2015-05-20.
+//  Copyright (c) 2015 Emily MacLeod. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
